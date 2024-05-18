a2ded0d3b2b459e58f98a10cd318b1a7c201c875
prashant94157 <0166621m@gmail.com>
2024-05-18 22:34:28 +0530

    fixing path

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://e-shop-client-rho.vercel.app/'],
+    methods: ['GET', 'POST', 'UPDATE', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
